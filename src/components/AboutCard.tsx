94800bb9cd5770518111a3492e2a129836069398
Raveen <raveenrv92@gmail.com>
2024-12-11 09:21:00 +0530

    About Card

--- src/components/AboutCard.tsx
+++ src/components/AboutCard.tsx
@@ -1,58 +1,24 @@
-import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
-import {
-  IconClipboardCopy,
-  IconFileBroken,
-  IconSignature,
-  IconTableColumn,
-} from "@tabler/icons-react";
+import { authStore } from "@/store/authStore";
+import { AnimatedTestimonials } from "./ui/animated-testimonials";
+import { useEffect } from "react";
 
 export function AboutCard() {
+  const { getAboutCard, aboutCard } = authStore();
+
+  useEffect(() => {
+    const fetch = async () => {
+      await getAboutCard();
+    };
+    fetch();
+  }, []);
+
   return (
-    <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
-      {items.map((item, i) => (
-        <BentoGridItem
-          key={i}
-          title={item.title}
-          description={item.description}
-          header={item.header}
-          className={item.className}
-          icon={item.icon}
-        />
-      ))}
-    </BentoGrid>
+    <>
+      {aboutCard ? (
+        <AnimatedTestimonials testimonials={aboutCard} />
+      ) : (
+        <div className="">No Card Found</div>
+      )}
+    </>
   );
 }
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
-);
-const items = [
-  {
-    title: "The Dawn of Innovation",
-    description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
-    className: "md:col-span-2",
-    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Digital Revolution",
-    description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
-    className: "md:col-span-1",
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Art of Design",
-    description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
-    className: "md:col-span-1",
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
-  },
-  {
-    title: "The Power of Communication",
-    description:
-      "Understand the impact of effective communication in our lives.",
-    header: <img src="head.png" className="w-full object-cover" alt="" />,
-    className: "md:col-span-2",
-    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
-  },
-];
